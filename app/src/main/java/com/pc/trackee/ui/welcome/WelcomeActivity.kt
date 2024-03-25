7421c406056841e4940adf853f3675950fef46ee
raismuhammad <muhammadrais96@gmail.com>
2024-03-25 20:45:08 +0700

    add welcome activity

---
+++ app/src/main/java/com/pc/trackee/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.pc.trackee.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pc.trackee.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
